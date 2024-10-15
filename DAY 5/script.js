139c19a9e4b316201ec1c99a2188bd7fa3c36594
Utkarsh-1541027 <utkarshsharma1306@gmail.com>
2024-10-15 15:36:58 +0530

    DAY5

---
+++ DAY 5/script.js
@@ -0,0 +1,11 @@
+//let x =confirm("Are you sure?");
+//alert(x);
+
+//let v= prompt("enter your name","Utk")
+//alert(v);
+
+
+let disp= () => {
+    console.log("welcomee to arrow functionn");
+}
+disp();
